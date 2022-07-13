dcc2d73138b498465130112135de800ff3729f78
zzCx5JZ1Ce <hackcer2003@gmail.com>
2022-07-13 17:44:12 +0700

    new

--- Do an/cloudflare-ddns-updater.sh
+++ Do an/cloudflare-ddns-updater.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 TOKEN="S3TfFa0rnkBeu0bfRIrIeDdTVhOGhA8GvxTOqgvN"
-ZONE_ID=fa7c1e086941561cb3eda0bb68bf6f11
-RECORD_NAME=sub.kamvdta.xyz
-ttl="1"
-proxy="false"
+ZONE_ID="fa7c1e086941561cb3eda0bb68bf6f11"
+RECORD_NAME="sub.kamvdta.xyz"
+TTL="1"
+PROXY="false"
 
 ipv4_regex='([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\.([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\.([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\.([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])'
 ip=$(curl -s -4 https://cloudflare.com/cdn-cgi/trace | grep -E '^ip'); ret=$?
@@ -17,5 +17,5 @@ while read id; do
     curl -X PUT https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${id} \
      -H "Authorization: Bearer ${TOKEN}" \
      -H "Content-Type: application/json" \
-	 --data "{\"type\":\"A\",\"name\":\"$RECORD_NAME\",\"content\":\"$ip\",\"ttl\":\"$ttl\",\"proxied\":${proxy}}")
+	 --data "{\"type\":\"A\",\"name\":\"$RECORD_NAME\",\"content\":\"$ip\",\"ttl\":\"$TTL\",\"proxied\":${PROXY}}"
 done)
